9b233394263894de07c70ba3b2b14b67d785d77d
Pawel Grochola <grocholapawel@gmail.com>
2017-11-13 11:00:49 +0100

    Reimplement SudokuReader using streams

--- src/pl/edu/agh/mownit/lab4/SudokuReader.java
+++ src/pl/edu/agh/mownit/lab4/SudokuReader.java
@@ -1,6 +1,8 @@
 package pl.edu.agh.mownit.lab4;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -18,37 +20,41 @@ public class SudokuReader {
 
     /**
      * Reads sudoku from file. Empty fields can be any token different from digits.
+     *
      * @return Sudoku with values from file
      * @throws SudokuFileException
      */
     public Sudoku readFromFile() throws SudokuFileException {
-        final File file = new File(fileName);
-        int rowNumber = 0;
-        final int[][] puzzle = new int[Sudoku.SIZE][Sudoku.SIZE];
+        final int[][] sudokuTable;
         final Set<String> possibleDigits = IntStream.range(1, Sudoku.SIZE + 1)
                 .mapToObj(Integer::toString)
                 .collect(Collectors.toSet());
 
-        try (final BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                if (rowNumber == Sudoku.SIZE) {
-                    throw new SudokuFileException(String.format("Too many rows in file %s", fileName));
-                }
-                final int[] sudokuRow = Arrays.stream(line.split("\\s+"))
-                        .map(digit -> possibleDigits.contains(digit) ? Integer.parseInt(digit) : 0)
-                        .mapToInt(Integer::intValue)
-                        .toArray();
-                if (sudokuRow.length != Sudoku.SIZE) {
-                    throw new SudokuFileException(String.format("Wrong number of digits in row %d in %s: '%s'", rowNumber, fileName, line));
-                }
-                puzzle[rowNumber] = sudokuRow;
-                rowNumber++;
-            }
+        try {
+            //returning an int[][] does not wo
+            sudokuTable = Files.lines(new File(fileName).toPath())
+                    .map(String::trim)
+                    .map(line -> line.split("\\s+"))
+                    .map(tokens -> Arrays.stream(tokens)
+                            .mapToInt(token -> possibleDigits.contains(token) ? Integer.parseInt(token) : 0)
+                            .toArray())
+                    .toArray(int[][]::new);
         } catch (IOException e) {
             throw new SudokuFileException(e);
         }
-        return new Sudoku(puzzle);
+        assertCorrectSudoku(sudokuTable);
+        return new Sudoku(sudokuTable);
+    }
+
+    private void assertCorrectSudoku(final int[][] sudokuTable) {
+        if (sudokuTable.length != Sudoku.SIZE) {
+            throw new SudokuFileException(String.format("Wrong number of lines in file '%s': %d", fileName, sudokuTable.length));
+        }
+        for (int[] row : sudokuTable) {
+            if (row.length != Sudoku.SIZE) {
+                throw new SudokuFileException(String.format("Too many digits in file '%s' line '%s': %d", fileName, Arrays.toString(row), row.length));
+            }
+        }
     }
 
     public static class SudokuFileException extends RuntimeException {
