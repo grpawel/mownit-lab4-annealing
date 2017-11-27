201da2d8332a4ff26c9b7211ea6c6d13ad1a697f
Pawel Grochola <grocholapawel@gmail.com>
2017-11-27 13:10:16 +0100

    Change generation of images

--- src/pl/edu/agh/mownit/lab4/problems/travellingsalesman/TSPImageCreator.java
+++ src/pl/edu/agh/mownit/lab4/problems/travellingsalesman/TSPImageCreator.java
@@ -19,12 +19,12 @@ public class TSPImageCreator {
     private BufferedImage createImage(final List<Point> points) {
         final int width = points.stream().mapToInt(point -> (int) point.getX()).max().getAsInt();
         final int height = points.stream().mapToInt(point -> (int) point.getY()).max().getAsInt();
-        final BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        final BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics2D graphics = image.createGraphics();
         for (int i = 0; i < points.size(); i++) {
             final Point point = points.get(i);
             final Point nextPoint = points.get((i + 1) % points.size());
-            graphics.drawOval(point.x, point.y, 5, 5);
+            graphics.fillOval(point.x, point.y, 5, 5);
             graphics.drawLine(point.x, point.y, nextPoint.x, nextPoint.y);
         }
         return image;
